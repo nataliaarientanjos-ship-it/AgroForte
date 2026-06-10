4f9b00502e26679ec3e721167bbec4c9e4489883
nataliaarientanjos-ship-it <natalia.arient.anjos@escola.pr.gov.br>
2026-06-10 15:09:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+document.getElementById("saibaMais").addEventListener("click", () => {
+    document.getElementById("sobre").scrollIntoView({
+        behavior: "smooth"
+    });
+});
 
+function animarContador(id, valorFinal, velocidade) {
+    let valor = 0;
+
+    const contador = setInterval(() => {
+        valor += Math.ceil(valorFinal / 100);
+
+        if (valor >= valorFinal) {
+            valor = valorFinal;
+            clearInterval(contador);
+        }
+
+        document.getElementById(id).textContent =
+            valor.toLocaleString('pt-BR');
+    }, velocidade);
+}
+
+window.addEventListener("load", () => {
+    animarContador("arvores", 50000, 30);
+    animarContador("agua", 1000000, 20);
+    animarContador("energia", 85, 40);
+});
